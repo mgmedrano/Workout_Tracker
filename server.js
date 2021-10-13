c37a2f04fc371db60462983310fa0d8d54c6cd7a
Michael Medrano <mgmedrano@outlook.com>
2021-10-12 22:30:05 -0700

    server.js filed updated include routes and mongoose connection

--- server.js
+++ server.js
@@ -3,9 +3,23 @@ const mongoose = require('mongoose');
 const morgan = require ('morgan');
 
 const app = express();
-const PORT = process.env.PORT || 
+const PORT = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+mongoose.connect(process.env.MONGODB_URI) || 'mongodb:/localhost/workout', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
+
+  app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
