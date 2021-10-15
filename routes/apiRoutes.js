36a56823231f042e2451d7a8dfd3a463e14c1f0a
Michael Medrano <mgmedrano@outlook.com>
2021-10-14 21:56:21 -0700

    route fixes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,30 +9,30 @@ module.exports = function(app) {
             res.status(400).json(err);
         });
     })
-
+    
     app.get("/api/workouts/range", ({}, res) => {
-        db.Workout.find({}).then(dbWorkout) => {
-            res.json(dbWorkout);
-        }).catch(err => {
-            res.status(400).json(err);
-        });
-    });
-
-    app.post("/api/workouts/", (req, res) => {
-        db.Workout.create(req, body).then(dbWorkout) => {
+      db.Workout.find({}).then((dbWorkout) => {
         res.json(dbWorkout);
       }).catch(err => {
         res.status(400).json(err);
       });
-  });
-
-    app.put("/api/workouts/:id", (req, res) => {
-        db.Workout.findByIdAndUpdate(
-            { _id: req.params.id }, { exercises: req.body }
-        ).then((dbWorkout) => {
-            res.json(dbWorkout);
+    });
+    
+    app.post("/api/workouts/", (req, res) => {
+        db.Workout.create(req.body).then((dbWorkout) => {
+          res.json(dbWorkout);
         }).catch(err => {
             res.status(400).json(err);
+          });
+      });
+    
+      app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findByIdAndUpdate(
+          { _id: req.params.id }, { exercises: req.body }
+        ).then((dbWorkout) => {
+          res.json(dbWorkout);
+        }).catch(err => {
+          res.status(400).json(err);
         });
     });
 };
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const logger = require ('morgan');
+const morgan = require ('morgan');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(logger("dev"));
+app.use(morgan("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb:/localhost/workout', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
