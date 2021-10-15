b6a4ec130d152086305135870a4c91cbf0094b74
Michael Medrano <mgmedrano@outlook.com>
2021-10-14 22:10:07 -0700

    Updated Routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,48 +2,37 @@ const db = require("../models");
 
 module.exports = function(app) {
     app.get("/api/workouts", (req, res) => {
-        db.Workout.aggregate([
-            {
-                $addFields: {
-                    totalDuration: {
-                        $sum: '$exercises.duration',
-                    },
-                },
-            },
-        ])
-        
-        
-        .then(dbWorkout => {
+        db.Workout.find({}).then(dbWorkout => {
             res.json(dbWorkout);
         })
         .catch(err => {
             res.status(400).json(err);
         });
     })
-    
-    app.get("/api/workouts/range", ({}, res) => {
+
+      app.get("/api/workouts/range", ({}, res) => {
       db.Workout.find({}).then((dbWorkout) => {
         res.json(dbWorkout);
       }).catch(err => {
         res.status(400).json(err);
       });
     });
-    
+
     app.post("/api/workouts/", (req, res) => {
-        db.Workout.create(req.body).then((dbWorkout) => {
-          res.json(dbWorkout);
+        db.Workout.create(req, body).then(dbWorkout) => {
+        res.json(dbWorkout);
+      }).catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+    app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findByIdAndUpdate(
+            { _id: req.params.id }, { exercises: req.body }
+        ).then((dbWorkout) => {
+            res.json(dbWorkout);
         }).catch(err => {
             res.status(400).json(err);
-          });
-      });
-    
-      app.put("/api/workouts/:id", (req, res) => {
-        db.Workout.findByIdAndUpdate(
-          { _id: req.params.id }, { exercises: req.body }
-        ).then((dbWorkout) => {
-          res.json(dbWorkout);
-        }).catch(err => {
-          res.status(400).json(err);
         });
     });
 };
