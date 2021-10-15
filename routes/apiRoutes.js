a2a901989a9a956075264daea1000916f05b9a70
Michael Medrano <mgmedrano@outlook.com>
2021-10-14 23:10:21 -0700

    Updated apiRoute

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,11 +5,9 @@ module.exports = function(app) {
         db.Workout.aggregate ([
             {
                 $addFields: {
-                    totalDuration: {
-                        $sum: '$exercises.duration',
-                    },
-                },
-            },
+                    totalDuration: { $sum: '$exercises.duration' }
+                }
+            }
         ])
         // find({})
 
@@ -41,8 +39,10 @@ module.exports = function(app) {
     
       app.put("/api/workouts/:id", (req, res) => {
         db.Workout.findByIdAndUpdate(
-          { _id: req.params.id }, { exercises: req.body }
-        ).then((dbWorkout) => {
+          { _id: req.params.id }, 
+          { $push: { exercises: req.body }
+        
+          // .then((dbWorkout) => {
           res.json(dbWorkout);
         }).catch(err => {
           res.status(400).json(err);
