3d54bf22c83109389c8435eec31a9cb7829755e8
Michael Medrano <mgmedrano@outlook.com>
2021-10-14 23:12:45 -0700

    revert code due to crash

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,16 +2,7 @@ const db = require("../models");
 
 module.exports = function(app) {
     app.get("/api/workouts", (req, res) => {
-        db.Workout.aggregate ([
-            {
-                $addFields: {
-                    totalDuration: { $sum: '$exercises.duration' }
-                }
-            }
-        ])
-        // find({})
-
-        .then(dbWorkout => {
+        db.Workout.find({}).then(dbWorkout => {
             res.json(dbWorkout);
         })
         .catch(err => {
@@ -39,10 +30,8 @@ module.exports = function(app) {
     
       app.put("/api/workouts/:id", (req, res) => {
         db.Workout.findByIdAndUpdate(
-          { _id: req.params.id }, 
-          { $push: { exercises: req.body }
-        
-          // .then((dbWorkout) => {
+          { _id: req.params.id }, { exercises: req.body }
+        ).then((dbWorkout) => {
           res.json(dbWorkout);
         }).catch(err => {
           res.status(400).json(err);
